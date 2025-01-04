b19cdf5e47c257ceae7eefb0db58f9afa34eaa9d
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2025-01-04 13:32:10 +0100

    feat: use nc for connection

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir "$HOME/.ssh"
+mkdir -p "$HOME/.ssh"
 
 if [ -n "$INPUT_SOCKS5_PWD" ]; then
   export SOCKS5_PASSWD="$INPUT_SOCKS5_PWD"
@@ -13,30 +13,30 @@ if [[ ! "$INPUT_PROXY_SERVER" =~ ^\d ]]; then
   INPUT_PROXY_SERVER=$(dig +short "$INPUT_PROXY_SERVER")
 fi
 
-config="$HOME/.ssh/config"
-
-echo "Host ${INPUT_HOST}" > "$config"
-echo "  User ${INPUT_USERNAME}" >> "$config"
-echo "  Port ${INPUT_PORT}" >> "$config"
-
 if [ -n "$INPUT_PROXY_USERNAME" ]; then
   INPUT_PROXY_USERNAME="$INPUT_PROXY_USERNAME@"
 fi
 
-echo "  ProxyCommand connect -S ${INPUT_PROXY_USERNAME}${INPUT_PROXY_SERVER}:${INPUT_PROXY_PORT} %h %p" >> "$config"
+cat >"$HOME/.ssh/config" <<EOL
+Host ${INPUT_HOST}
+    User ${INPUT_USERNAME}
+    Port ${INPUT_PORT}
+    ProxyCommand nc -X connect -x ${INPUT_PROXY_USERNAME}${INPUT_PROXY_SERVER}:${INPUT_PROXY_PORT} %h %p
+EOL
 
 if [ -z "$INPUT_KEY" ]
 then
   echo "Using password"
+
   export SSHPASS="$PASS"
   sshpass -e ssh -o StrictHostKeyChecking=accept-new "$INPUT_HOST" "$INPUT_RUN"
 else
   echo "Using private key"
+
   echo "$INPUT_KEY" > "$HOME/.ssh/id_rsa"
   chmod 400 "$HOME/.ssh/id_rsa"
 
-  echo "  IdentityFile $HOME/.ssh/id_rsa" >> "$config"
-  cat "$HOME/.ssh/config"
+  echo "    IdentityFile $HOME/.ssh/id_rsa" >> "$HOME/.ssh/config"
 
   ssh -o StrictHostKeyChecking=accept-new "$INPUT_HOST" "$INPUT_RUN"
 fi
